6e33294fdfbfaa18719f23823161265fc9c98ad4
Mohammed Khaled Amine Ali <133232872+Demi-Khaled@users.noreply.github.com>
2024-10-11 03:58:54 +0300

    Update Projects.jsx

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -13,7 +13,7 @@ function Projects() {
          <div className={styles.projectsContainer}>
           <ProjectCard src={trendy} link={"https://github.com/Demi-Khaled/Trendy"} h3="Trendy" p="Clothing Store Management System"/>
           <ProjectCard src={mobizil} link={"https://github.com/Demi-Khaled/mobizil"} h3="Mobizil" p="Mobile Review and News Platform"/>
-          <ProjectCard src={workWave} link={"https://github.com/Demi-Khaled/WorkWave"} h3="Mobizil" p="Freelancing App"/>
+          <ProjectCard src={workWave} link={"https://github.com/Demi-Khaled/WorkWave"} h3="WorkWave" p="Freelancing App"/>
           <ProjectCard src={secureCrypt} link={"#"} h3="CyberCrypt" p="Encryption app"/>
         </div>
       </section>
